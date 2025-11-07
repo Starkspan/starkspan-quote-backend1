6d8a054b238a310935f04aafcf29b7ee1a6d16ba
Starkspan <info@kuntsolutions.de>
2025-11-07 14:18:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ const { createWorker } = require("tesseract.js");
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
+
 app.use(express.json());
 
 // ------- Preis-/Dichte-Tabellen -------
